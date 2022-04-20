d500174c61161b4cf034c25810283f11bcccecef
Batin EVIRGEN <batinevirgen@gmail.com>
2022-04-20 20:49:09 +0300

    feat(ui): update issue read page (spaghetti warning)

--- pages/comic/[comicSlug]/[issueSlug]/index.tsx
+++ pages/comic/[comicSlug]/[issueSlug]/index.tsx
@@ -4,19 +4,65 @@ import {
   GetStaticPropsContext,
   NextPage,
 } from 'next';
+import { useRouter } from 'next/router';
 import { ParsedUrlQuery } from 'querystring';
-import { getAllComics, getIssueBySlug } from '~/lib/database';
-import { IIssueDocument } from '~/lib/database/models';
+import { Button } from '~/components/Button';
+import { Container } from '~/components/Container';
+import { Reader } from '~/components/Reader';
+import { getAllComics, getComicBySlug, getIssueBySlug } from '~/lib/database';
+import { IComicDocument, IIssueDocument } from '~/lib/database/models';
 import { callDb } from '~/lib/utils/database';
 
 interface IIssueSlugPageProps {
   issue: IIssueDocument;
+  comic: IComicDocument;
+  nextIssue: IIssueDocument | null;
+  prevIssue: IIssueDocument | null;
 }
 
 const IssueSlugPage: NextPage<IIssueSlugPageProps> = ({
   issue,
+  comic,
+  nextIssue,
+  prevIssue,
 }: IIssueSlugPageProps) => {
-  return <div>Issue slug page</div>;
+  const router = useRouter();
+  const goNextIssue = () => {
+    if (nextIssue) {
+      router.push(`/comic/${comic.slug}/${nextIssue.slug}`);
+    } else {
+      router.push(`/comic/${comic.slug}`);
+    }
+  };
+
+  const issueNavDOM = (
+    <Container className="flex gap-4 justify-center my-2 ">
+      {prevIssue && (
+        <Button
+          type="default"
+          href={`/comic/${comic.slug}/${prevIssue.slug}`}
+          text="Previous Issue"
+        />
+      )}
+      {nextIssue && (
+        <Button
+          type="default"
+          href={`/comic/${comic.slug}/${nextIssue.slug}`}
+          text="Next Issue"
+        />
+      )}
+    </Container>
+  );
+
+  return (
+    <div>
+      <h1 className="text-4xl font-medium text-center">{`${comic.name}`}</h1>
+      <h2 className="text-2xl text-center">{issue.name}</h2>
+      {issueNavDOM}
+      <Reader images={issue.images as string[]} onFinished={goNextIssue} />
+      {issueNavDOM}
+    </div>
+  );
 };
 
 interface IStaticPathsQuery extends ParsedUrlQuery {
@@ -62,11 +108,27 @@ export const getStaticProps: GetStaticProps<
   const comicSlug = slugs.comicSlug;
   const issueSlug = slugs.issueSlug;
 
+  //TODO: We could do all of these in (probably) one getComicBySlug call.
+  const comic = await callDb(
+    getComicBySlug(comicSlug, 'name slug issues', ['issues']),
+    true
+  );
+
+  const comicIssues = comic.issues as IIssueDocument[];
   const issue = await callDb(getIssueBySlug(comicSlug, issueSlug), true);
+
+  const issueIndex = comicIssues.findIndex((i) => i._id == issue._id);
+  const nextIssue = issueIndex === 0 ? null : comicIssues[issueIndex - 1];
+  const prevIssue =
+    issueIndex === comicIssues.length - 1 ? null : comicIssues[issueIndex + 1];
 
   return {
     props: {
       issue,
+      comic,
+      nextIssue,
+      prevIssue,
+      key: issue._id,
     },
   };
 };
