9972a13d73719226b71fa4d0ed441107fb6a3589
Batin EVIRGEN <batinevirgen@gmail.com>
2022-04-28 15:40:34 +0300

    fix(ui): add unknown type to issue.comic

--- components/LatestIssues/SingleIssue.tsx
+++ components/LatestIssues/SingleIssue.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image';
 import Link from 'next/link';
-import { IIssueDocument } from '~/lib/database/models';
+import { IComicDocument, IIssueDocument } from '~/lib/database/models';
 import { toHumanReadable } from '~/lib/utils/date';
 
 interface ISingleIssueProps {
@@ -9,8 +9,10 @@ interface ISingleIssueProps {
 
 const SingleIssue = ({ issue }: ISingleIssueProps) => {
   const coverImage = (issue.images as string[])[0];
+  //TODO: Remove unknown from here.
+  const comic = issue.comic as unknown as IComicDocument;
   return (
-    <Link href={`/comic/${issue.comic.slug}/${issue.slug}`}>
+    <Link href={`/comic/${comic.slug}/${issue.slug}`}>
       <a className="group flex mb-2 bg-neutral-900 hover:bg-neutral-800 rounded-md transition duration-100 sm:mb-0">
         <div className="mr-2 w-1/2">
           <Image
@@ -25,9 +27,7 @@ const SingleIssue = ({ issue }: ISingleIssueProps) => {
         </div>
         <div className="flex flex-col justify-between py-2 w-1/2">
           <div>
-            <p className="font-medium group-hover:text-white">
-              {issue.comic.name}
-            </p>
+            <p className="font-medium group-hover:text-white">{comic.name}</p>
             <p className="group-hover:text-white">{issue.name}</p>
           </div>
           <p className="text-sm">{toHumanReadable(issue.createdAt as Date)}</p>
