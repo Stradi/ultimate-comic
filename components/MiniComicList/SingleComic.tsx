2afb9bc37aca1b2156251f03d0332ea8c47021cd
Batin EVIRGEN <batinevirgen@gmail.com>
2022-05-01 00:09:37 +0300

    style: lint

--- components/MiniComicList/SingleComic.tsx
+++ components/MiniComicList/SingleComic.tsx
@@ -9,7 +9,7 @@ interface ISingleComic {
 const SingleComic = ({ comic, addViewCount }: ISingleComic) => {
   return (
     <Link href={`/comic/${comic.slug}`}>
-      <a className="relative group block p-2 my-1 truncate bg-neutral-900 hover:bg-neutral-800 rounded-md focus:outline-none focus:ring-2 focus:ring-red-600 transition duration-100 sm:my-0">
+      <a className="group block relative p-2 my-1 truncate bg-neutral-900 hover:bg-neutral-800 rounded-md focus:outline-none focus:ring-2 focus:ring-red-600 transition duration-100 sm:my-0">
         {comic.isCompleted ? (
           // eslint-disable-next-line tailwindcss/no-custom-classname
           <i className="group-hover:text-green-500 align-middle transition duration-100 ri-check-line ri-fw" />
@@ -21,8 +21,8 @@ const SingleComic = ({ comic, addViewCount }: ISingleComic) => {
           {comic.name}
         </span>
         {addViewCount && (
-          <div className="absolute top-1/2 bg-black/90 w-full h-full left-0 -translate-y-1/2 opacity-0 group-hover:opacity-100 transition duration-100">
-            <span className="absolute left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2 text-sm">
+          <div className=" absolute top-1/2 left-0 w-full h-full bg-black/90 opacity-0 group-hover:opacity-100 transition duration-100 -translate-y-1/2">
+            <span className="absolute top-1/2 left-1/2 text-sm -translate-x-1/2 -translate-y-1/2">
               Total Views:{' '}
               <span className="font-medium text-red-600">
                 {comic.totalViews || 0}
