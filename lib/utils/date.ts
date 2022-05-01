d6c1a35dd8cf35c78d60673e2847d00579d96629
Batin EVIRGEN <batinevirgen@gmail.com>
2022-05-01 17:02:54 +0300

    fix: change date conversion from local to en-US

--- lib/utils/date.ts
+++ lib/utils/date.ts
@@ -1,6 +1,6 @@
 const toHumanReadable = (date: Date) => {
   const d = new Date(date.toString());
-  return d.toLocaleDateString(undefined, {
+  return d.toLocaleDateString(['en-US'], {
     day: '2-digit',
     month: 'long',
     year: 'numeric',
