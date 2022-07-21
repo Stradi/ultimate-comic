71d9645b51fae8a2fddb7f004df142f5b3ec1331
Batin EVIRGEN <batinevirgen@gmail.com>
2022-07-21 22:04:20 +0300

    feat: add className prop to Button component

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -1,41 +1,49 @@
 import Link from 'next/link';
 
+import cx from 'classnames';
+
 interface IButtonProps {
   href: string;
   text: string;
   type: 'default' | 'minimal';
+  className?: string;
 }
 
 interface IButtonTypeProps {
   href: string;
   text: string;
+  className?: string;
 }
 
-const DefaultButton = ({ href, text }: IButtonTypeProps) => {
+const DefaultButton = ({ href, text, className }: IButtonTypeProps) => {
+  const classes = cx(
+    'py-2 px-4 text-white bg-red-600 hover:bg-red-800 rounded-md focus:outline-none focus:ring-2 focus:ring-red-600 transition-colors duration-100',
+    className
+  );
   return (
     <Link href={href}>
-      <a className="py-2 px-4 text-white bg-red-600 hover:bg-red-800 rounded-md focus:outline-none focus:ring-2 focus:ring-red-600 transition-colors duration-100">
-        {text}
-      </a>
+      <a className={classes}>{text}</a>
     </Link>
   );
 };
 
-const MinimalButton = ({ href, text }: IButtonTypeProps) => {
+const MinimalButton = ({ href, text, className }: IButtonTypeProps) => {
+  const classes = cx(
+    'py-2 px-4 hover:text-white rounded-md border-2 border-neutral-800 hover:border-red-800 focus:outline-none focus:ring-2 focus:ring-red-600 transition-colors duration-100',
+    className
+  );
   return (
     <Link href={href}>
-      <a className="py-2 px-4 hover:text-white rounded-md border-2 border-neutral-800 hover:border-red-800 focus:outline-none focus:ring-2 focus:ring-red-600 transition-colors duration-100">
-        {text}
-      </a>
+      <a className={classes}>{text}</a>
     </Link>
   );
 };
 
-const Button = ({ href, text, type }: IButtonProps) => {
+const Button = ({ href, text, type, className }: IButtonProps) => {
   if (type === 'default') {
-    return <DefaultButton href={href} text={text} />;
+    return <DefaultButton href={href} text={text} className={className} />;
   } else {
-    return <MinimalButton href={href} text={text} />;
+    return <MinimalButton href={href} text={text} className={className} />;
   }
 };
 
