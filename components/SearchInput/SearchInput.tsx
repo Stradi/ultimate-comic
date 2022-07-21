472385456b7bdd4a3bf9af0cfc172c9aec79d1ea
Batin EVIRGEN <batinevirgen@gmail.com>
2022-07-21 22:06:57 +0300

    feat: add displayButton prop to SearchInput

--- components/SearchInput/SearchInput.tsx
+++ components/SearchInput/SearchInput.tsx
@@ -1,22 +1,47 @@
 import { ChangeEventHandler } from 'react';
+import { Button } from '../Button';
 
 interface ISearchInputProps {
-  onChange: ChangeEventHandler<HTMLInputElement>;
+  onChange?: ChangeEventHandler<HTMLInputElement>;
   initialValue?: string;
+  displayButton?: boolean;
+  buttonHref?: string;
+  buttonText?: string;
+  buttonType?: 'minimal' | 'default';
 }
 
-const SearchInput = ({ onChange, initialValue }: ISearchInputProps) => {
+const SearchInput = ({
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  onChange,
+  initialValue,
+  displayButton = false,
+  buttonHref = '',
+  buttonText = 'Search',
+  buttonType = 'default',
+}: ISearchInputProps) => {
   return (
-    <div className="flex relative items-center my-8 mx-auto w-1/3 h-full">
-      <input
-        type={'text'}
-        className="absolute top-0 left-0 p-2 pl-9 w-full placeholder:font-medium placeholder:text-neutral-700 text-neutral-300 focus:placeholder:text-neutral-500 bg-neutral-800 rounded-md focus:outline-none ring-2 ring-neutral-700 focus:ring-red-600 transition duration-100"
-        placeholder="Search..."
-        onChange={onChange}
-        value={initialValue || ''}
-      />
-      {/* eslint-disable-next-line tailwindcss/no-custom-classname */}
-      <i className="z-10 py-3 px-2 font-medium text-neutral-500 ri-search-line ri-lg" />
+    <div className="w-full">
+      <div className="flex justify-center my-8 mx-auto w-full h-full">
+        <div className="flex relative w-full md:w-1/3">
+          <input
+            type={'text'}
+            className="absolute top-0 left-0 p-2 pl-9 w-full placeholder:font-medium placeholder:text-neutral-700 text-neutral-300 focus:placeholder:text-neutral-500 bg-neutral-800 rounded-md focus:outline-none ring-2 ring-neutral-700 focus:ring-red-600 transition duration-100"
+            placeholder="Search..."
+            onChange={onChange || undefined}
+            defaultValue={initialValue && initialValue}
+          />
+          {/* eslint-disable-next-line tailwindcss/no-custom-classname */}
+          <i className="z-10 py-3 px-2 font-medium text-neutral-500 ri-search-line ri-lg" />
+        </div>
+        {displayButton && (
+          <Button
+            href={buttonHref}
+            text={buttonText}
+            type={buttonType}
+            className="ml-2"
+          />
+        )}
+      </div>
     </div>
   );
 };
