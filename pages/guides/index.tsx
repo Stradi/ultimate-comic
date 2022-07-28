cce1598fefb164ca2042850990873fa2fa2cbf0b
Batin EVIRGEN <batinevirgen@gmail.com>
2022-07-28 13:34:52 +0300

    feat: add /guides page for listing all guides

---
+++ pages/guides/index.tsx
@@ -0,0 +1,48 @@
+import { GetStaticProps, NextPage } from 'next';
+import { NextSeo } from 'next-seo';
+import { Container } from '~/components/Container';
+import { GuideList } from '~/components/GuideList';
+import { getAllGuides } from '~/lib/utils/blog';
+import { handle } from '~/lib/utils/promise';
+
+interface IGuideListPageProps {
+  guides: GuidePage[];
+}
+
+// TODO: Add pagination
+const GuideListPage: NextPage<IGuideListPageProps> = ({
+  guides,
+}: IGuideListPageProps) => {
+  return (
+    <Container>
+      <NextSeo
+        title="Guides"
+        description="Don't know where to start reading a comic. Check out these guides for specific characters and events."
+      />
+      <h1 className="block mb-2 text-lg font-medium text-center text-white">
+        All Guides
+      </h1>
+      {guides.length > 0 ? (
+        <div>
+          <GuideList guides={guides} />
+        </div>
+      ) : (
+        <div className="text-center">
+          <p className="text-xl font-medium">There are no guides, yet.</p>
+        </div>
+      )}
+    </Container>
+  );
+};
+
+export const getStaticProps: GetStaticProps<IGuideListPageProps> = async () => {
+  const [, allGuides] = await handle(getAllGuides());
+
+  return {
+    props: {
+      guides: allGuides ? JSON.parse(JSON.stringify(allGuides)) : [],
+    },
+  };
+};
+
+export default GuideListPage;
