a76a5260d0cdd7b6888aabcb7d7b42ad595e6e48
Batin EVIRGEN <batinevirgen@gmail.com>
2022-07-30 18:18:59 +0300

    fix: add xs tailwind breakpoint and fix some responsiveness issues

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
 module.exports = {
   content: ['./pages/**/*.{ts,tsx}', './components/**/*.{ts,tsx}'],
   theme: {
-    extend: {},
+    screens: {
+      xs: '475px',
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1536px',
+    },
   },
   plugins: [
     require('@tailwindcss/typography'),
