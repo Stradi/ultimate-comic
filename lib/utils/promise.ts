5d4433b3ee2abe3ac15008807683980fe9039bc0
Batin EVIRGEN <batinevirgen@gmail.com>
2022-08-01 22:34:42 +0300

    feat: catch error in 'handle<T,U>(p: Promise<T>)' function since it's used everywhere

--- lib/utils/promise.ts
+++ lib/utils/promise.ts
@@ -1,9 +1,12 @@
+import * as Sentry from '@sentry/nextjs';
+
 const handle = <T, U = Error>(
   promise: Promise<T>
 ): Promise<[U, undefined] | [null, T]> => {
   return promise
     .then<[null, T]>((data: T) => [null, data])
-    .catch<[U, undefined]>((err: U) => {
+    .catch<[U, undefined]>(async (err: U) => {
+      await Sentry.captureException(err);
       return [err, undefined];
     });
 };
