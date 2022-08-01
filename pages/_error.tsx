91dd52ef0ec39def1c89c0a9eb02e2b3fbaa28f0
Batin EVIRGEN <batinevirgen@gmail.com>
2022-08-01 15:43:33 +0300

    fix: remove 500.tsx page and create _error.tsx page to capture errors

---
+++ pages/_error.tsx
@@ -0,0 +1,32 @@
+import * as Sentry from '@sentry/nextjs';
+import { NextPageContext } from 'next';
+import NextErrorComponent from 'next/error';
+import { Button } from '~/components/Button';
+import { Container } from '~/components/Container';
+
+interface ICustomErrorComponentProps {
+  statusCode: number;
+}
+
+const CustomErrorComponent = ({ statusCode }: ICustomErrorComponentProps) => {
+  return (
+    <Container>
+      <div className="text-center align-middle">
+        <h1 className="text-9xl font-black text-white">{statusCode}</h1>
+        <p className="mb-8 text-7xl">Something happened in our side.</p>
+        <p className="mb-8 text-4xl">We are investigating the problem.</p>
+        <div className="flex justify-center gap-2 text-2xl">
+          <Button href="/" text="Home" type="default" />
+          <Button href="/all-comics" text="All Comics" type="minimal" />
+        </div>
+      </div>
+    </Container>
+  );
+};
+
+CustomErrorComponent.getInitialProps = async (contextData: NextPageContext) => {
+  await Sentry.captureUnderscoreErrorException(contextData);
+  return NextErrorComponent.getInitialProps(contextData);
+};
+
+export default CustomErrorComponent;
