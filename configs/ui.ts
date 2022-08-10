3f9e0c864565de28ea0c34bebb4308b8be55afaa
Batin EVIRGEN <batinevirgen@gmail.com>
2022-08-10 14:10:34 +0300

    fix: change comicPerPage option for allComics and tag pages to reduce image count in pages

--- configs/ui.ts
+++ configs/ui.ts
@@ -1,9 +1,9 @@
 const PAGES = {
   ALL_COMICS: {
-    COMIC_PER_PAGE: process.env.NODE_ENV === 'development' ? 20 : 100,
+    COMIC_PER_PAGE: process.env.NODE_ENV === 'development' ? 20 : 50,
   },
   TAG: {
-    COMIC_PER_PAGE: process.env.NODE_ENV === 'development' ? 20 : 100,
+    COMIC_PER_PAGE: process.env.NODE_ENV === 'development' ? 20 : 50,
     GENERATE_ON_BUILD: process.env.NODE_ENV === 'development' ? 10 : 50,
   },
   COMIC: {
