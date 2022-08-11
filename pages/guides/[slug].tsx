00cef85ef106774034a162866e0a5157db6cc504
Batin EVIRGEN <batinevirgen@gmail.com>
2022-08-11 16:50:02 +0300

    fix: update /guide/[slug] page

--- pages/guides/[slug].tsx
+++ pages/guides/[slug].tsx
@@ -12,26 +12,74 @@ import { getAllGuides, getGuideBySlug } from '~/lib/utils/blog';
 import { handle } from '~/lib/utils/promise';
 
 import MDXComponents from '~/components/MDXComponents';
+import { toHumanReadable } from '~/lib/utils/date';
+import { CardList } from '~/components/CardList';
+import { comicToCardListProp } from '~/components/CardList/CardList.helper';
+import { callDb } from '~/lib/utils/database';
+import { getComicBySlug } from '~/lib/database';
+import { IComicDocument } from '~/lib/database/models';
 
 interface IGuidePageProps {
   guide: GuidePage;
+  relatedComics: IComicDocument[];
 }
 
-const GuidePage: NextPage<IGuidePageProps> = ({ guide }: IGuidePageProps) => {
+const GuidePage: NextPage<IGuidePageProps> = ({
+  guide,
+  relatedComics,
+}: IGuidePageProps) => {
   return (
-    <Container>
-      <NextSeo
-        title={`${guide.title} Guide`}
-        description={guide.seo.description}
-      />
-      <h1 className="text-center text-3xl font-black text-white">
-        {guide.title}
-      </h1>
-      <br></br>
-      <div className="prose prose-invert mx-auto prose-a:text-inherit prose-a:transition prose-a:duration-100 hover:prose-a:text-red-600 prose-blockquote:border-l-red-600 prose-li:marker:font-bold prose-li:marker:text-red-600">
-        <MDXRemote {...guide.content} components={MDXComponents} />
-      </div>
-    </Container>
+    <div className="flex">
+      <Container>
+        <NextSeo
+          title={`${guide.title} Guide`}
+          description={guide.seo.description}
+        />
+        <h1 className="text-center text-5xl font-bold text-white">
+          {guide.title} Guide
+        </h1>
+        <br></br>
+        <div className="flex flex-col text-center text-sm">
+          <p>
+            <span className="text-neutral-400">By </span>
+            <span className="font-medium text-neutral-100">
+              UltimateComic staff
+            </span>
+          </p>
+          <p>
+            Published at{' '}
+            <span className="font-medium text-neutral-100">
+              {toHumanReadable(guide.publishedAt)}
+            </span>
+            , last updated at{' '}
+            <span className="font-medium text-neutral-100">
+              {toHumanReadable(guide.updatedAt)}
+            </span>
+          </p>
+        </div>
+        <br></br>
+        <div className="prose-lg prose prose-invert mx-auto prose-h1:mb-2 prose-a:text-inherit prose-a:transition prose-a:duration-100 hover:prose-a:text-red-600 prose-blockquote:border-l-red-600 prose-li:marker:font-bold prose-li:marker:text-red-600">
+          <p>{guide.excerpt}</p>
+          <MDXRemote {...guide.content} components={MDXComponents} />
+        </div>
+      </Container>
+      <aside className="sticky inset-y-0 mx-4 hidden h-full rounded-lg bg-neutral-900 p-4 sm:block">
+        <div>
+          <h2 className="mb-4 text-2xl font-medium text-white">
+            Related Comics
+          </h2>
+          <div className="h-screen overflow-y-auto py-0.5">
+            <CardList
+              items={relatedComics.map((relatedComic) =>
+                comicToCardListProp(relatedComic)
+              )}
+              responsive={false}
+              singleColumn={true}
+            />
+          </div>
+        </div>
+      </aside>
+    </div>
   );
 };
 
@@ -66,9 +114,22 @@ export const getStaticProps: GetStaticProps<
     };
   }
 
+  const relatedComics = [];
+  for (const relatedComic of guide.relatedComics) {
+    const [error, comic] = await callDb(
+      handle(getComicBySlug(relatedComic, 'name slug coverImage issues')),
+      true
+    );
+
+    if (!error && comic !== null) {
+      relatedComics.push(comic as IComicDocument);
+    }
+  }
+
   return {
     props: {
       guide: JSON.parse(JSON.stringify(guide)),
+      relatedComics,
     },
   };
 };
