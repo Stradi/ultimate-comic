9ca6ac8f4effbef49f6217363fc1f4ebaecaff46
Batin EVIRGEN <batinevirgen@gmail.com>
2022-08-22 14:09:43 +0300

    fix: update /blog/[slug] page

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -13,6 +13,7 @@ import { getAllPosts, getBlogPostBySlug } from '~/lib/utils/blog';
 import { handle } from '~/lib/utils/promise';
 
 import MDXComponents from '~/components/MDXComponents';
+import { toHumanReadable } from '~/lib/utils/date';
 
 interface IBlogSlugPageProps {
   post: BlogPost;
@@ -34,11 +35,30 @@ const BlogSlugPage: NextPage<IBlogSlugPageProps> = ({
         url={`${SEO.URL}/blog/${post.slug}`}
         images={[]}
       />
-      <h1 className="text-center text-3xl font-black text-white">
+      <h1 className="text-center text-5xl font-bold text-white">
         {post.title}
       </h1>
       <br></br>
-      <div className="prose prose-invert mx-auto prose-a:text-inherit prose-a:transition prose-a:duration-100 hover:prose-a:text-red-600 prose-blockquote:border-l-red-600 prose-li:marker:font-bold prose-li:marker:text-red-600">
+      <div className="flex flex-col text-center text-sm">
+        <p>
+          <span className="text-neutral-400">By </span>
+          <span className="font-medium text-neutral-100">
+            UltimateComic staff
+          </span>
+        </p>
+        <p>
+          Published at{' '}
+          <span className="font-medium text-neutral-100">
+            {toHumanReadable(post.publishedAt)}
+          </span>
+          , last updated at{' '}
+          <span className="font-medium text-neutral-100">
+            {toHumanReadable(post.updatedAt)}
+          </span>
+        </p>
+      </div>
+      <br></br>
+      <div className="prose-lg prose prose-invert mx-auto max-w-3xl prose-h1:mb-2 prose-a:text-inherit prose-a:transition prose-a:duration-100 hover:prose-a:text-red-600 prose-blockquote:border-l-red-600 prose-li:marker:font-bold prose-li:marker:text-red-600 prose-img:my-0 prose-img:p-2">
         <MDXRemote {...post.content} components={MDXComponents} />
       </div>
     </Container>
