171f0d9e2a0c71d9ab1259dc427f31f9ae13f511
Batin EVIRGEN <batinevirgen@gmail.com>
2022-08-24 11:40:13 +0300

    fix: use Section component in /genres page

--- pages/genres.tsx
+++ pages/genres.tsx
@@ -3,6 +3,7 @@ import { NextSeo } from 'next-seo';
 import { CardList } from '~/components/CardList';
 import { tagToCardListProps } from '~/components/CardList/CardList.helper';
 import { Container } from '~/components/Container';
+import { Section } from '~/components/Section';
 import { getAllTags } from '~/lib/database';
 import { ITagDocument } from '~/lib/database/models';
 import { callDb } from '~/lib/utils/database';
@@ -19,13 +20,16 @@ const GenresPage: NextPage<IGenresPageProps> = ({ tags }: IGenresPageProps) => {
         description={`All genres available in UltimateComic website. With more than ${tags.length} genres you can easily find any comic you want.`}
       />
       <Container>
-        <h1 className="mb-2 block text-center text-lg font-medium text-white">
-          Most Popular Genres
-        </h1>
-        <CardList
-          items={tags.map((tag) => tagToCardListProps(tag))}
-          responsive={false}
-        />
+        <Section
+          title="All Genres"
+          subtitle="List of all genres from UltimateComic"
+          showSeeAllButton={false}
+        >
+          <CardList
+            items={tags.map((tag) => tagToCardListProps(tag))}
+            responsive={false}
+          />
+        </Section>
       </Container>
     </>
   );
